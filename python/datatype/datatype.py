d00f10b2e67a8e0a90c27a812e3d3e2caaabf741
kawaishun <shun@osstech.co.jp>
2017-06-14 16:52:13 +0900

    wrote datatype.py

--- python/datatype/datatype.py
+++ python/datatype/datatype.py
@@ -1,1 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# datatype.py
+
+# bool
+
+a = True
+b = False
+
+print("datatype [bool]")
+print("a =",a)
+print("b =",b)
+print("type(a) =",type(a))
+print("\n")
+
+# int and float
+
+a = 1
+b = 2.0
+
+print("datatype [int]")
+print("a =",a)
+print("type(a) =",type(a))
+print("\n")
+
+print("datatype [float]")
+print("b =",b)
+print("type(b) =",type(b))
+print("\n")
+
+c = a+b
+print("c = a+b")
+print("c =",c)
+print("type(c)",type(c))
+print("\n")
+
+print("11  /  2 = ",11/2)
+print("11  // 2 = ",11//2)
+print("-11 // 2 = ",-11//2)
+print("11  ** 2 = ",11**2)
+print("11  %  2 = ",11%2)
+
 
