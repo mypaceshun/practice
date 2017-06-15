b81e91980bdcf070ceba63d352c6a60e44c3bb6c
kawaishun <shun@osstech.co.jp>
2017-06-15 13:52:45 +0900

    function

--- bin/setup.sh
+++ bin/setup.sh
@@ -20,5 +20,6 @@ echo $_SHUN_SHELL_HOME
 echo 'export SHUN_SHELL_HOME'
 echo 'SHUN_SHELL_HOME='$_SHUN_SHELL_HOME >> $HOME/.bash_profile
 echo 'export SHUN_SHELL_HOME' >> $HOME/.bash_profile
+echo 'export PATH=$PATH:$SHUN_SHELL_HOME/bin' >> $HOME/.bash_profile
 source $HOME/.bash_profile
 
---
+++ python/simple/function.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# auther: shun kawai
+
+#===============================
+# function.py
+#===============================
+
+def func(var):
+  print(var)
+
+func("test")
