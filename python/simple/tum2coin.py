24bcd188a62f254461c4076bff1cab5bd9e5541d
shun <shun@osstech.co.jp>
2017-06-27 13:52:55 +0900

    function.py

--- python/simple/tum2coin.py
+++ python/simple/tum2coin.py
@@ -1,15 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# vi: set expandtab shiftwidth=4 :
 
 # auther: shun kawai
 
 #===============================
-# function.py
+# tum2coin.py
 #===============================
 
-def func(a,b):
-  print(a,"+",b,"=",a+b)
 
-for x in range(1,11):
-  func(x, x*x)
